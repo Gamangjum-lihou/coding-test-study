46960c97c26e70407cb695a03a3dfe616e784d9c
minjaeKim <smallkdb@gmail.com>
2023-01-09 17:01:14 +0900

    feat: 김민재 코드 제출

---
+++ 과일장수/김민재.js
@@ -0,0 +1,10 @@
+function solution(k, m, score) {
+	let answer = 0;
+	score.sort((a, b) => b - a);
+	for (let i = 0; i < score.length; i += m) {
+		if (i + m <= score.length) {
+			answer += Math.min.apply(null, score.slice(i, i + m)) * m;
+		}
+	}
+	return answer;
+}
