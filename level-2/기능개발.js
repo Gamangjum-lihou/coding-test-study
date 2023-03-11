8e58c26c0bf023fb563e4468dbe8079d8ccfc52f
SWARVY <swarvy0826@naver.com>
2023-03-11 22:18:49 +0900

    edit : 신현호 문제 설명 변경

--- level-2/기능개발.js
+++ level-2/기능개발.js
@@ -16,26 +16,26 @@ function solution(progresses, speeds) {
   const answer = [];
   const end = progresses.length - 1; // 종료 index
   let index = 0; // 시작 index
-  let day = 0;
+  let count = 0; // 배포되는 개수
 
   while (index <= end) {
     // 진행도가 100이 되지 않았다면
     if (progresses[index] < 100) {
-      // 첫날이 아니라면
-      if (day !== 0) {
-        answer.push(day);
-        day = 0;
+      // 완료된 작업들이 있다면
+      if (count !== 0) {
+        answer.push(count); // 한번의 배포될 분량 push
+        count = 0; // count 초기화
       }
       for (let j = 0; j <= end; j++) progresses[j] += speeds[j];
     } else {
       // 진행도가 100 이상이면 패스하고 인덱스를 넘어감
-      day++;
-      index++;
+      count++; // 배포 개수 1++
+      index++; // 다음 인덱스로 이동
     }
   }
 
-  // 종료되고나서 마지막 날짜 push
-  if (day !== 0) answer.push(day);
+  // 마지막 남은 작업에 대한 값 push
+  if (count !== 0) answer.push(count);
   return answer;
 }
 
