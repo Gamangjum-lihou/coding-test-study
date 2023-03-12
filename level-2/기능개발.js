65393b2ffa14d250a1f897b6ae2b2a5876fdaf30
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-03-12 15:54:48 +0900

    feat : 기능개발 - 강철원 풀이 추가

--- level-2/기능개발.js
+++ level-2/기능개발.js
@@ -4,6 +4,56 @@
  * 강철원
  */
 
+function solution(progresses, speeds) {
+  const result = [];
+  // 각 요일 마다 배포 가능한 개수
+  let deployCount = 0;
+  // 배포되는데 필요한 일수  
+  let maxDay = Math.ceil((100 - progresses[0]) / speeds[0]);
+
+  for (let i = 0; i < progresses.length; i++) {
+    // 개별 작업의 배포되는데 필요한 일수 
+    const day = Math.ceil((100 - progresses[i]) / speeds[i]);
+
+    // 앞 작업에서 배포에 필요한 일수보다 뒷작업의 일수가 더 적으면 함께 배포된다.     
+    if (day <= maxDay) {
+      deployCount++;
+    } else {
+      // 앞 작업에서 배포 일수를 충족하였는데 뒷 작업이 배포 할 수 없는 상황일 때      
+      result.push(deployCount);
+      deployCount = 1;
+      maxDay = day;
+    }
+  }
+
+  result.push(deployCount);
+  return result;
+}
+
+// 틀린 풀이
+
+function solution(progresses, speeds) {
+    const result = []
+
+    while(progresses.length > 0) {
+       const deployStack = []
+       // 제일 앞의 작업이 배포하는데 필요한 일수
+       const day = Math.ceil((100 - progresses[0]) / speeds[0]);
+      // 제일 앞의 작업이 배포가능한 일수만큼 시간이 흐름 
+        progresses = progresses.map((value, index) => value + speeds[index] * day)
+      // 작업 진도 100 이하 일때 for문 중지
+        for(let i=0; i<progresses.length; i++) {
+            if(progresses[i] < 100) break
+            deployStack.push(progresses[i])
+        }
+      // 해당 날짜에 배포 가능한 작업 개수
+        result.push(deployStack.length)
+      // 배포된 작업은 progresses에서 제거 
+        progresses = progresses.slice(deployStack.length)
+    }
+    return result
+}
+
 /*
  * 김민재
  */
