bed8d88f882645cfe0aadd8b1d4e0ed1c74f6548
SWARVY <swarvy0826@naver.com>
2023-04-01 22:49:40 +0900

    docs : 링크 수정

--- level-2/카펫.js
+++ level-2/카펫.js
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/42842#qna
+// https://school.programmers.co.kr/learn/courses/30/lessons/42842
 
 /*
  * 강철원
--- level-2/피로도.js
+++ level-2/피로도.js
@@ -1,4 +1,4 @@
-// 링크
+// https://school.programmers.co.kr/learn/courses/30/lessons/87946
 
 /*
  * 강철원
