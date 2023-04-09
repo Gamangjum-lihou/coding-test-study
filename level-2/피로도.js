70ae482caef7f5e16b01a713e5d3d9994fdceab7
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-04-09 18:35:02 +0900

    Add : 피로도 풀이(강철원)

--- level-2/피로도.js
+++ level-2/피로도.js
@@ -4,6 +4,29 @@
  * 강철원
  */
 
+let answer = 0;
+
+function solution(k, dungeons) {
+    const check = Array.from(dungeons, i => 0)
+    DFS(k, 0, dungeons, check);
+    return answer;
+}
+
+
+function DFS(k, cnt, dungeons, check) {
+    // cnt : 탐험한 던전 개수, k: 남은 피로도
+  answer = Math.max(answer, cnt);
+  for (let i = 0; i < dungeons.length; i++) {
+      // ["최소 필요 피로도", "소모 피로도"]
+      // 경로 첫 방문이면서 남은 피로도가 최소 필요 피로도 보다 크다면
+    if (check[i] === 0 && k >= dungeons[i][0]) {
+      check[i] = 1; // 던전 방문 했음 표시
+      DFS(k - dungeons[i][1], cnt + 1, dungeons, check); // 다음 던전 으로 이동
+      check[i] = 0; // 현재 방문한 던전 이후 다시 이전 던전으로 되돌아가서 다른 던전 이동을 위해 초기화
+    }
+  }
+}
+
 /*
  * 김민재
  */
