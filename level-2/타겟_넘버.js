6bb3a14777587b2b927c6fcfff0a58d99b451a3f
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-04-22 18:48:28 +0900

    feat : 타켓넘버-강철원

--- level-2/타겟_넘버.js
+++ level-2/타겟_넘버.js
@@ -4,6 +4,40 @@
  * 강철원
  */
 
+function solution(numbers, target) {
+    let answer = 0;
+    dfs(0, 0);
+    function dfs(index, sum) {
+        // numbers 의 length 까지 합을 구한뒤 그게 target하고 같으면 answer++
+        if(index === numbers.length) {
+            if (sum === target) answer++;
+            return;
+        }
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    return answer;
+}
+
+// 다른사람 풀이
+
+function solution(numbers, target) {
+    let answer = 0;
+    getAnswer(0,0);
+    function getAnswer(x,value) {
+        if(x<numbers.length){
+            getAnswer(x+1,value + numbers[x]);
+            getAnswer(x+1,value - numbers[x]);
+        } else{
+            if(value === target){
+                answer++
+            }
+        }
+    }
+    return answer;
+}
+
+
 /*
  * 신현호
  */
