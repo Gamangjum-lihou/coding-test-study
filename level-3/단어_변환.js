8ccfdf899783b3cda191364f78bdc7c0097e7791
Bori <85009583+Bori-github@users.noreply.github.com>
2023-04-30 19:36:12 +0900

    feat: 단어 변환 풀이 - 이보리

--- level-3/단어_변환.js
+++ level-3/단어_변환.js
@@ -42,3 +42,43 @@ function bfs(begin, target, words) {
  * 이보리
  */
 
+function solution(begin, target, words) {
+    const visited = [];
+    let queue = [];
+    
+    // words에 target이 없으면 단어를 변환할 수 없으므로 0을 반환한다.
+    if (!words.includes(target)) return 0;
+    
+    // begin(root)과 deps를 큐에 넣어준다.
+    queue.push([begin, 0]);
+    
+    while (queue.length !== 0) {
+        // 현재 단어와 deps를 꺼낸다.
+        const [currentWord, deps] = queue.shift();
+        
+        // 현재 단어가 target과 일치하면 deps를 반환한다.
+        if(currentWord === target) return deps;
+        
+        // 현재 단어와 words의 각 단어를 비교한다.
+        words.forEach((word) => {
+            if(isOneCharDifference(currentWord, word) && !visited.includes(word)) {
+                visited.push(word); // 방문 처리
+                queue.push([word, deps + 1]); // 단어와 deps를 queue에 넣어준다.
+            }
+        })
+    }
+    
+    // words를 전부 순회 했는데도 단어를 변환할 수 없다면 0 반환한다.
+    return 0;
+}
+
+// 한 번에 한 개의 알파벳만 바꿀 수 있는지 확인
+const isOneCharDifference = (word1, word2) => {
+    let count = 0;
+    
+    for(let i = 0; i < word1.length; i++) {
+        if(word1[i] !== word2[i]) count++;
+    }
+
+    return count === 1 ? true : false;
+}
