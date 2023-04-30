fe824f8da60ed3f7dd556d2e35a09c6d5a03d29d
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-04-30 18:39:14 +0900

    feat : 네트워크_풀이(강철원)

--- level-3/네트워크.js
+++ level-3/네트워크.js
@@ -4,6 +4,32 @@
  * 강철원
  */
 
+
+function solution(n, computers) {
+    const visited = [];
+    let answer = 0;
+
+    // 연결되어있는 것끼리 계속 콜백을해서 방문표시 
+    function dfs(i) {
+        visited[i] = true;
+        for(let j=0; j<computers[i].length; j++) {
+            // 컴퓨터가 서로 연결되어있고 방문한적이 없었을 때
+            if(computers[i][j] && !visited[j]){
+                dfs(j);
+            }
+        }
+    }
+    
+    for (let i=0; i < computers.length; i++) {
+        // 방문되지 않았을 때
+        if (!visited[i]) {
+            dfs(i)
+            answer++;
+        }
+    }
+    return answer;
+}
+
 /*
  * 신현호
  */
