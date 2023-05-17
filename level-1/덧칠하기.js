9b983a263a3a1f6b8e644fd7afdebe5a85cf2006
Bori <85009583+Bori-github@users.noreply.github.com>
2023-05-17 14:44:03 +0900

    feat: 덧칠하기 풀이 - 이보리

--- level-1/덧칠하기.js
+++ level-1/덧칠하기.js
@@ -12,6 +12,20 @@
  * 이보리
  */
 
+function solution(n, m, section) {
+    let count = 0;
+    let printed = 0;
+    
+    section.forEach((sectionNumber) => {
+        if(sectionNumber > printed) {
+            count++;
+            printed = sectionNumber + m - 1;
+        }
+    })
+    
+    return count;
+}
+
 /*
  * 채희수
  */
