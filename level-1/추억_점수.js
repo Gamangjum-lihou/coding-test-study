fd10a8c4f58be949b3fb7bb2bf7f0dacdc2700b0
Bori <85009583+Bori-github@users.noreply.github.com>
2023-05-17 14:43:16 +0900

    feat: 추억 점수 풀이 - 이보리

--- level-1/추억_점수.js
+++ level-1/추억_점수.js
@@ -12,6 +12,28 @@
  * 이보리
  */
 
+function solution(name, yearning, photo) {
+    const answer = [];
+    const scoreMap = new Map();
+    
+    for (let i = 0; i < name.length; i++) {
+        scoreMap.set(name[i], yearning[i]);
+    };
+    
+    photo.forEach((friends) => {
+        const score = friends
+            .filter((friend) => scoreMap.has(friend))
+            .map((freind) => scoreMap.get(freind));
+        
+        // 사진에 그리워하는 인물이 없는 경우 totalScore = 0 처리
+        const totalScore = score.length > 0 ? score.reduce((acc, cur) => acc + cur) : 0;
+        
+        answer.push(totalScore);
+    })
+    
+    return answer;
+}
+
 /*
  * 채희수
  */
