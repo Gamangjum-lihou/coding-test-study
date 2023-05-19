927b0514d82be9900cbfbda17e25ea194d03ab07
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-05-19 12:49:37 +0900

    feat : 추억점수_강철원 풀이 추가

--- level-1/추억_점수.js
+++ level-1/추억_점수.js
@@ -4,6 +4,39 @@
  * 강철원
  */
 
+function solution(name, yearning, photo) {
+    // 생각 
+    // name + yearning  객체로 만들어서 계속 활용
+    
+    const longingBox = {}
+
+    
+    // 1. 객체로 만들기
+    // 두개 동시에 해야하기에 forEach나 reduce보단 for문으로 한 번에 두개를 넣자 
+    // 어차피 name과 yearing의 길이는 같으니까
+    for( let i = 0 ; i<name.length; i++) {
+        longingBox[name[i]] = yearning [i]
+    }
+    
+    const toto = name.reduce((acc,cur,index) => {
+        acc[cur] =  yearning[index]
+        return acc
+    },{})
+    console.log(toto)
+    
+    // 2. longingBox를 활용해서 photo의 이중배열을 계산 및 처리후 result 배열 만들기 
+    
+    return photo.reduce((photoAcc, photoCur) => {
+        const longingScore = photoCur.reduce((acc,cur, index, arr) => {
+            // 그리움 상자에 해당 인물이 있을 때        
+            if(longingBox[cur]) acc += longingBox[cur]
+            return acc
+    },0)
+        photoAcc.push(longingScore)
+        return photoAcc
+    },[])
+}
+
 /*
  * 신현호
  */
