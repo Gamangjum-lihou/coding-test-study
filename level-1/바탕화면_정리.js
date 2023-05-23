238edc2d87eb83780675ae4306f1c3acb1d11019
Bori <85009583+Bori-github@users.noreply.github.com>
2023-05-23 13:47:33 +0900

    feat: 바탕화면 정리 풀이 - 이보리

--- level-1/바탕화면_정리.js
+++ level-1/바탕화면_정리.js
@@ -12,6 +12,29 @@
  * 이보리
  */
 
+function solution(wallpaper) {
+    // '#' 상수로 지정
+    const findChar = '#';
+    
+    // 각 배열 초기화
+    const x = []; // 세로 좌표 배열
+    const luy = []; // 가로 좌표 시작점 배열
+    const rdy = []; // 가로 좌표 끝점 배열
+    
+    // wallpaper을 순회
+    wallpaper.forEach((wall, index) => {
+        // findChar를 포함하는 경우에만 값을 추가
+        if (wall.includes(findChar)) {
+            x.push(index);
+            luy.push(wall.indexOf(findChar));
+            rdy.push(wall.lastIndexOf(findChar));
+        }
+    });
+
+    // [lux, luy, rdx, rdy]
+    return [Math.min(...x), Math.min(...luy), Math.max(...x) + 1, Math.max(...rdy) + 1];
+}
+
 /*
  * 채희수
  */
