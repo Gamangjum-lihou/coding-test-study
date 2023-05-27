53c2f0a147ad83f2f1fa8b8ebebf79556b87f7f6
cochae <100351462+chee9835@users.noreply.github.com>
2023-05-27 19:44:22 +0900

    feat: 바탕화면 정리 풀이 - 채희수

--- level-1/바탕화면_정리.js
+++ level-1/바탕화면_정리.js
@@ -38,3 +38,24 @@ function solution(wallpaper) {
 /*
  * 채희수
  */
+
+function solution(wallpaper) {
+    // 좌표마다 가질 수 있는 범위를 구한다
+    var xRange = [];
+    var yRange = [];
+    
+    
+    for (let i in wallpaper) {
+        if (wallpaper[i].indexOf('#') !== -1) {
+            xRange.push(+i);
+            yRange.push(wallpaper[i].indexOf('#'));
+            yRange.push(wallpaper[i].lastIndexOf('#'));
+        }
+    }
+    
+    // y좌표는 정렬이 필요하다.
+    yRange.sort((a,b) => a-b);
+    
+    // 최솟값과 최댓값을 각각 리턴한다. 파일값까지 드래그 해야하기 때문에 1을 더해준다.
+    return [xRange[0], yRange[0], xRange[xRange.length-1]+1, yRange[yRange.length-1]+1];
+}
