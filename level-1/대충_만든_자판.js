6587a4243f26d3434e0ea7d3b6b2105aea86702a
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-05-28 17:50:06 +0900

    feat : 대충 만든 자판 풀이 - 강철원

--- level-1/대충_만든_자판.js
+++ level-1/대충_만든_자판.js
@@ -4,6 +4,35 @@
  * 강철원
  */
 
+function solution(keymap, targets) {
+  let count = 0; // 해당 알파벳을 작성하기 위해 키를 최소 몇 번 눌러야하는지 횟수
+  const result = [];
+
+  // 1. 최소 keymap 객체  ( 각 알파벳별로 최소로 누를 수 있는 횟수 저장)
+  const keymapBox = keymap.reduce((acc, cur) => {
+    [...cur].forEach((alphabet, index) => {
+      //  해당 알파벳의 최소로 누를 수 있는 횟수 < index + 1
+      // 최소로 누를 수 있는 횟수가 1부터이니 index + 1
+      acc[alphabet] = acc[alphabet] < index + 1 ? acc[alphabet] : index + 1;
+    });
+    return acc;
+  }, {});
+
+  // 2. targets에 있는 각 알파벳을 keymapBox를 통해 각 target별로 최소 몇 번씩 눌러야 하는지 계산
+  for (let i = 0; i < targets.length; i++) {
+    for (let j = 0; j < targets[i].length; j++) {
+      // count += 해당 알파벳의 횟수
+      count += keymapBox[targets[i][j]];
+    }
+    // 목표 문자열을 작성할 수 없을 때는 -1을 push
+    result.push(count ? count : -1);
+    // count 초기화
+    count = 0;
+  }
+  return result;
+}
+
+
 /*
  * 신현호
  */
