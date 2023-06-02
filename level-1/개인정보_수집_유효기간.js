c8017c934e715d1424469fbca429a8a72091e986
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-06-02 22:12:57 +0900

    feat: 개인정보 수집 유효기간-강철원

--- level-1/개인정보_수집_유효기간.js
+++ level-1/개인정보_수집_유효기간.js
@@ -4,6 +4,38 @@
  * 강철원
  */
 
+function solution(today, terms, privacies) {
+  today = new Date(today.replace(/[.]/g, '-'));
+
+  const updatedTerms = convertArrayToObject(terms);
+
+  return privacies.reduce((acc, cur, index) => {
+    const expirationDate = getExpirationDate(cur, updatedTerms);
+    if (isExpired(expirationDate, today)) acc.push(index + 1);
+    return acc;
+  }, []);
+}
+
+function convertArrayToObject(terms) {
+  return terms.reduce((acc, cur) => {
+    const [type, month] = cur.split(' ');
+    acc[type] = month;
+    return acc;
+  }, {});
+}
+
+function getExpirationDate(cur, updatedTerms) {
+  const [date, type] = cur.split(' ');
+  const joinDate = new Date(date.replace(/[.]/g, '-'));
+  joinDate.setMonth(joinDate.getMonth() + +updatedTerms[type]);
+  return joinDate;
+}
+
+function isExpired(expirationDate, today) {
+  return expirationDate <= today;
+}
+
+
 /*
  * 신현호
  */
