28e21631cb827e838c6ad84e0d1b7be4b9fee480
cochae <huisu121@gmail.com>
2023-06-21 21:56:43 +0900

    feat: 영어 끝말잇기 풀이

--- level-2/영어_끝말잇기.js
+++ level-2/영어_끝말잇기.js
@@ -15,3 +15,27 @@
 /*
  * 채희수
  */
+
+function solution(n, words) {
+  let person = 0;
+  let turn = 0;
+  const wordSet = new Set(words[0]);
+  console.log(wordSet);
+
+  for (let i = 1; i < words.length; i++) {
+    if (wrongWord(words[i - 1], words[i]) || wordSet.has(words[i])) {
+      const order = +i + 1;
+      person = order % n === 0 ? n : order % n;
+      turn = Math.ceil(order / n);
+      break;
+    }
+
+    wordSet.add(words[i]);
+  }
+
+  return [person, turn];
+}
+
+function wrongWord(prev, next) {
+  return prev.at(prev.length - 1) !== next.at(0);
+}
