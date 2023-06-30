53de8d6776516e93b560df9724b3fb353e70e933
Bori <85009583+Bori-github@users.noreply.github.com>
2023-06-30 13:27:26 +0900

    feat: 구명보트 풀이

--- level-2/구명보트.js
+++ level-2/구명보트.js
@@ -12,6 +12,23 @@
  * 이보리
  */
 
+function solution(people, limit) {
+    let answer = 0;
+    
+    people.sort((a, b) => a - b);
+    
+    while(people.length > 0) {
+        answer += 1;
+        
+        if (people.at(0) + people.at(-1) <= limit) {
+            people.shift();
+        }
+        people.pop();
+    }
+    
+    return answer;
+}
+
 /*
  * 채희수
  */
