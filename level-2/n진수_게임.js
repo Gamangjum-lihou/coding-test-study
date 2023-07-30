11fd9f142b513b5897e52201fa57cf9f39ddc92b
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-07-30 16:33:15 +0900

    feat : n진수 게임 풀이

--- level-2/n진수_게임.js
+++ level-2/n진수_게임.js
@@ -4,6 +4,30 @@
  * 강철원
  */
 
+function solution(n, t, m, p) {
+  let index = 0;
+  let numbers = '';
+  let result = '';
+
+// 미리 구할 숫자의 개수를 만족하면 while 종료
+  while (result.length < t) {
+    const limitLengthOfNumbers = t * m;
+    if (numbers.length < limitLengthOfNumbers) {
+      // n 진법으로 변경
+      numbers += index.toString(n).toUpperCase();
+    }
+
+    const order = (index % m) + 1;
+    const isTubeTurn = order === p;
+    if (isTubeTurn) {
+      result += numbers[index];
+    }
+
+    index += 1;
+  }
+  return result;
+}
+
 /*
  * 이보리
  */
