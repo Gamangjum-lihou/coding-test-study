17c52e66e046037f0fa9ae731a02996f348a0dd8
Cheol-Won <76567238+Ryan-Dia@users.noreply.github.com>
2023-08-06 16:59:13 +0900

    feat : 주차 요금 계산 풀이

--- level-2/주차_요금_계산.js
+++ level-2/주차_요금_계산.js
@@ -4,6 +4,74 @@
  * 강철원
  */
 
+function solution(fees, records) {
+  const parkingData = new ParkingData(fees, records);
+  return parkingData.getResult();
+}
+
+class ParkingData {
+  #fees;
+
+  #parkingData;
+
+  #result;
+
+  constructor(fees, records) {
+    this.#fees = fees;
+    this.records = records;
+    this.#parkingData = {};
+    this.#run();
+  }
+
+  #run() {
+    this.#calculateTotalHoursOfUse();
+    this.#sort();
+    this.#calculateFee();
+  }
+
+  getResult() {
+    return this.#result;
+  }
+
+  #calculateTotalHoursOfUse(carData = {}) {
+    this.records.forEach((record) => {
+      let [passTime, carNumber, inOrOut] = record.split(' ');
+      let [hour, minute] = passTime.split(':');
+      passTime = hour * 60 + +minute;
+      this.#parkingData[carNumber] = this.#parkingData[carNumber] ?? 0;
+
+      if (inOrOut === 'IN') return (carData[carNumber] = passTime);
+      this.#parkingData[carNumber] += passTime - carData[carNumber];
+      return (carData[carNumber] = null);
+    });
+    this.#processCarNotPulledOut(carData);
+  }
+
+  #processCarNotPulledOut(carData) {
+    const DEFULAT_OUT_TIME = 1439;
+    for (let carNumber in carData) {
+      if (carData[carNumber] !== null) {
+        this.#parkingData[carNumber] += DEFULAT_OUT_TIME - carData[carNumber];
+      }
+    }
+  }
+
+  #sort() {
+    this.#parkingData = Object.entries(this.#parkingData).sort(([a], [b]) => +a - +b);
+  }
+
+  #calculateFee() {
+    const [basicTime, basicFee, unitTime, unitFee] = this.#fees;
+    this.#result = this.#parkingData.map(([_, stayTime]) => {
+      let divide = (stayTime - basicTime) / unitTime;
+      divide < 1 ? (divide = 1) : (divide = Math.ceil(divide));
+      if (stayTime > basicTime) return basicFee + divide * unitFee;
+      return basicFee;
+    });
+  }
+}
+
+
 /*
  * 이보리
  */
