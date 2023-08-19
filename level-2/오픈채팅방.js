8d3f88363edf210445274efa435d240b692053ad
Bori <85009583+Bori-github@users.noreply.github.com>
2023-08-20 00:28:01 +0900

    feat : 오픈채팅방 풀이

--- level-2/오픈채팅방.js
+++ level-2/오픈채팅방.js
@@ -8,6 +8,42 @@
  * 이보리
  */
 
+const TYPE = {
+    ENTER: "Enter",
+    LEAVE: "Leave",
+    CHANGE: "Change",  
+}
+
+const MESSAGE = {
+    ENTER: "님이 들어왔습니다.",
+    LEAVE: "님이 나갔습니다.",
+}
+
+function solution(record) {
+    // record의 각 요소를 문자열에서 배열 형태로 변환
+    const records = record.map((message) => message.split(" "));
+    const userMap = new Map();
+    
+    // records를 순회하면서 유저 아이디에 따른 닉네임 Map을 생성
+    records.forEach(([type, userId, nickname]) => {
+        // 채팅방을 나간 경우 아무런 변화가 없다.
+        if(type === TYPE.LEAVE) return;
+        
+        // 채팅방을 입장했거나 닉네임을 변경했을 때 userMap에 유저 아이디와 닉네임을 매핑하여 저장
+        userMap.set(userId, nickname);
+    })
+    
+    // 사용자가 닉네임을 변경했을 경우 아무런 메시지를 나타내지 않으므로 filter를 이용하여 걸러낸다.
+    const result = records.filter(([type]) => type !== TYPE.CHANGE)
+        .map(([type, userId]) => {
+            // 각 type에 따른 메시지를 userMap에 저장된 사용자 닉네임과 조합하여 반환한다.
+            if(type === TYPE.ENTER) return userMap.get(userId) + MESSAGE.ENTER;
+            if(type === TYPE.LEAVE) return userMap.get(userId) + MESSAGE.LEAVE;
+        })
+    
+    return result;
+}
+
 /*
  * 신현호
  */
