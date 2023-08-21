49f27fba5bc55a7b7f33e27ffe6b52d5920cc472
Bori <85009583+Bori-github@users.noreply.github.com>
2023-08-21 14:12:09 +0900

    feat : 땅따먹기 풀이

--- level-2/땅따먹기.js
+++ level-2/땅따먹기.js
@@ -28,6 +28,24 @@ function solution(land) {
  * 이보리
  */
 
+function solution(land) {
+    const COLUMNS_NUMBER = 4
+
+    // land를 순회하면서 같은 열의 숫자를 연속으로 선택하지 않은 각 숫자의 합 배열 생성 
+    const sum = land.reduce((acc, cur) => {
+        return cur.map((number, index) => {
+            // 이 전 행에서 현재 인덱스의 숫자를 제외
+            const filter = acc.filter((_, accIndex) => accIndex !== index);
+            // 현재 숫자와 이전 행에서 현재 인덱스의 숫자를 제외한 숫자 중 최대값 더하기
+            return number + Math.max(...filter);
+        })
+    // 열의 개수만큼 0으로 초기화 된 배열을 초기값으로 적용
+    }, Array.from({ length: COLUMNS_NUMBER }, () => 0));
+
+    // 계산된 합의 최대값을 반환
+    return Math.max(...sum);
+}
+
 /*
  * 신현호
  */
