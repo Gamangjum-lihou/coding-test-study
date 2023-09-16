b1474be1fa3277b0ebb5acb4cc0980c4061cb941
Bori <85009583+Bori-github@users.noreply.github.com>
2023-09-17 00:12:37 +0900

    feat: 두 큐 합 같게 만들기 풀이

--- level-2/두_큐_합_같게_만들기.js
+++ level-2/두_큐_합_같게_만들기.js
@@ -8,6 +8,50 @@
  * 이보리
  */
 
+const getSum = (arr) => {
+    return arr.reduce((acc, cur) => acc + cur, 0);
+}
+
+function solution(queue1, queue2) {
+    // 두 개의 큐를 하나의 큐로 붙이기
+    const totalQueue = [...queue1, ...queue2];
+    const sumOfTotalQueue = getSum(totalQueue);
+    
+    // 두 큐에 담긴 모든 원소의 합이 홀수 인 경우, -1 반환
+    if(sumOfTotalQueue % 2 !== 0) return -1;
+    
+    // 각 큐의 원소 합
+    const sumOfEachQueue = sumOfTotalQueue / 2;
+    // 최대 작업 수
+    const maxCount = queue1.length * 3;
+    
+    let sum = getSum(queue1);
+    let startIndex = 0;
+    let endIndex = queue1.length;
+    
+    for (let count = 0; count < maxCount; count++) {                
+        // 각 큐의 원소 합이 같아지는 경우 count를 반환
+        if (sum === sumOfEachQueue) return count;
+        
+        // queue1 원소 합이 각 큐의 원소 합보다 작은 경우
+        if (sum < sumOfEachQueue) {
+            // totalQueue의 endIndex에 해당하는 원소 값을 더한다.
+            sum += totalQueue[endIndex];
+            // endIndex를 다음 인덱스로 이동 시킨다.
+            endIndex += 1;
+        }
+        // queue1 원소 합이 각 큐의 원소 합보다 큰 경우
+        else if (sum > sumOfEachQueue) {
+            // totalQueue의 startIndex에 해당하는 원소 값을 뺀다.
+            sum -= totalQueue[startIndex];
+            // startIndex를 다음 인덱스로 이동 시킨다.
+            startIndex += 1;
+        }
+    }
+    // 각 큐의 원소 합을 같게 만들 수 없는 경우, -1을 반환
+    return -1;
+}
+
 /*
  * 신현호
  */
