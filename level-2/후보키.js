9866328c238b28bd33e792eb7129daa2b209f677
Bori <85009583+Bori-github@users.noreply.github.com>
2023-09-30 16:11:45 +0900

    feat: 후보키 풀이

--- level-2/후보키.js
+++ level-2/후보키.js
@@ -8,6 +8,72 @@
  * 이보리
  */
 
+const getCombinations = (arr, length) => {
+    if (length === 1) return arr.map((value) => [value]);
+    
+    const result = arr.reduce((acc, cur, index, origin) => {
+        const rest = origin.slice(index + 1);
+        const combinations = getCombinations(rest, length - 1);
+        const attached = combinations.map(combination => [cur, ...combination]);
+        
+        acc.push(...attached);
+        return acc;
+    }, []);
+    
+    return result;
+}
+
+// 조합에 따른 문자열 키 반환
+const getKeys = (combination) => {
+    // 문자열 키를 만들기 위해 배열을 빈 문자열로 초기화
+    let keys = Array.from({ length: combination[0].length }, () => '');
+
+    // 각 조합에 따른 문자열 키를 생성
+    for (let j = 0; j < combination.length; j++) {
+        for (let k = 0; k < combination[0].length; k++) {
+            keys[k] += combination[j][k];
+        }
+    }
+    
+    return keys;
+}
+
+// 유일성 확인
+const isUniqueness = (combination, relation) => {
+    const keys = getKeys(combination);
+    const keysSet = new Set(keys);
+
+    return keysSet.size === relation.length;
+}
+
+function solution(relation) {
+    let answer = [];
+    // relation의 행과 열의 위치를 변경, 열을 기준으로 하는 2차원 배열 
+    const columns = relation.reduce((acc, cur) => {
+        return cur.map((_, index) => [...(acc[index] || []), cur[index]])
+    }, []);
+
+    // columns의 길이만큼 순회
+    for (let i = 1; i <= columns.length; i++) {
+        // 조합을 구함
+        const combinations = getCombinations(columns, i);;
+        // 각 조합을 순회하면서 하나의 문자열 키로 만듬
+        combinations.forEach(combination => {
+            // 유일성 체크
+            if (isUniqueness(combination, relation)) {
+                const combinationIndices = combination.map(value => columns.indexOf(value)); 
+                const isMinimality = !answer.some(value => value.every(el => combinationIndices.includes(el)));
+                // 최소성 체크
+                if(isMinimality) {
+                    answer.push(combinationIndices);
+                }
+            }
+        })
+    }
+
+    return answer.length;
+}
+
 /*
  * 신현호
  */
